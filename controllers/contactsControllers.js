0110c317cc79e4f848ba5d87ec37440ccf0f43e9
drankstar <137785834+drankstar@users.noreply.github.com>
2024-05-12 21:11:39 +0300

    done

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -78,7 +78,7 @@ export const updateStatusContact = async (req, res) => {
       next(HttpError(404))
     }
     res.send(updateStatusContact)
-  } catch (erro) {
+  } catch (error) {
     next(HttpError)
   }
 }
